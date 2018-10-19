77266a3f445197b1ec77846784f00844344dd0f2
ansara.hooks <ansara.hooks@gmail.com>
2018-10-19 18:17:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ function sayHiToGrandma(string) {
   if (string === 'hello') {
     return "I can't hear you!";
   }
-  else if (string.toUpperCase() === "HELLO") {
+  else if (string === "HELLO") {
     return "YES INDEED!";
   }
   else (string ==="I love you, Grandma."); {
